4432f39d8257e512baa962f07bd56c0af1f56545
Mr.TEN <34998996+vBathuN@users.noreply.github.com>
2024-05-25 22:32:12 +0530

    Update rtcs-script.js

--- images/rtcs-script.js
+++ images/rtcs-script.js
@@ -1,5 +1,5 @@
 var index = 0;
-var data= ["Web 3.0", "The Real Time Communication Server", "Web 3.0", "The Real Time Broadcasting Server", "Web 3.0", "The Real Time Communication Services"];
+var data= ["Web 3.0", "The Real Time Communication Services", "Web 3.0", "The Real Time Broadcasting Server", "Web 3.0", "The Real Time Communication Server", "No Downloads Required, No Personal Data Collected, No Sign-in Needed", "Compatible With Most Modern Browsers and Mobile Devices", "Video data is peer-to-peer, so unlike Zoom, your video data does not go thru the NSA's spying servers", "Low latency. I'm talking as low as 30-ms, and normally it never goes higher than 200-ms", "Video can be transferred over a LAN directly, so if using your phone as a webcam, you can crank the bitrates up to 40-mbps if you want, and your bandwidth won't be affected"];
 
 var span= document.querySelector('span');
 var section= document.querySelector('section');
